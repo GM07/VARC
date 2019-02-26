6000165ee330761e5368a0f51aea1dbb6692704f
PyrrozM <simon.daze@gmail.com>
2019-02-26 14:36:14 -0500

    resolution de conflits

--- src/convolutional/neural/network/MaxPoolingLayer.java
+++ src/convolutional/neural/network/MaxPoolingLayer.java
@@ -4,14 +4,13 @@ import math.Matrix;
 
 public class MaxPoolingLayer extends CNNLayer {
 
-	protected double stride;
 	protected int filterSize;
 	protected int poolingSize;
 
-	public void MaxPoolingLayer(int size, int nbFilters, int st, int poolSize) {
+	public void MaxPoolingLayer(int size, int nbFilters, int poolSize) {
 		filterSize = size;
 		filters = new Filter[nbFilters];
-		stride = st;
+
 		poolingSize = poolSize;
 		for(int i = 0; i < nbFilters; i++ ) {
 			filters[i] = new Filter(filterSize,filterSize);
@@ -21,7 +20,7 @@ public class MaxPoolingLayer extends CNNLayer {
 	public Matrix[] operation(Matrix[] inputs){
 		Matrix[] out = new Matrix[filters.length];
 		for(int i = 0; i < inputs.length; i++) {
-			out[i] = inputs[i].maxPool(poolingSize, stride);
+			out[i] = inputs[i].maxPool(poolingSize);
 		}
 		return out;
 	}
