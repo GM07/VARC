0b314e3982f2496b6f1b181ff4fa87a91cf27a9a
PyrrozM <simon.daze@gmail.com>
2019-03-19 22:42:15 -0400

    fonction softmax et sa derivee

---
+++ src/functions/SoftmaxFunction.java
@@ -0,0 +1,57 @@
+package functions;
+
+import math.Matrix;
+
+/**
+ * Classe de classification a l'aide d'une fonction qui tansforme des outputs en probabilites 
+ * @author Simon Daze
+ * 
+ *
+ */
+public class SoftmaxFunction {
+	
+	/**
+	 * Applique la fonction softmax a un vecteur pour en ressortir des probabilites
+	 * @param logits le vecteur 
+	 * @param index l'element du vecteur a evalue
+	 * @return la probabilite associee a l'element du vecteur en index
+	 */
+	public static double getValue( Matrix logits , int index) {
+		double eSum = 0;
+		for (int  i = 0 ; i < logits.getROWS(); i++) {
+			eSum+= Math.exp(logits.getElement(i, 0));
+		}
+		
+		return (Math.exp(logits.getElement(index, 0) )/ eSum);
+	}
+	
+	/**
+	 * Evalue la derivee dela fonction softmax pour un element d'un vecteur
+	 * @param index l'index de l'element ou la derivee est evaluee
+	 * @param logits l'element du vecteur
+	 * @param results la probabilite associee a cet element
+	 * @return la derivee de softmax relative a cet element
+	 */
+	public static double getDerivative(int index, Matrix logits, Matrix results) {
+		return (getValue(results,index)*(1-getValue(logits,index)));
+	}
+	
+	public static void main(String[] args) {
+		Matrix logits = new Matrix(3,1);
+		Matrix results = new Matrix(3,1);
+		logits.setElement(0, 0, 2);
+		logits.setElement(1, 0, 1);
+		logits.setElement(2, 0, 0.1);
+		results.setElement(0, 0, getValue(logits,0));
+		results.setElement(1, 0, getValue(logits,1));
+		results.setElement(2, 0, getValue(logits,2));
+		System.out.println(logits);
+		System.out.println(results);
+	/* testDerivative = getDerivative(0,logits,results);
+		System.out.println(testDerivative);
+		*/
+	}
+		
+
+	
+}
